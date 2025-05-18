fce8a5ea8890592e9f7ed524a68a09a42f4f8950
pete gamache <pete@gamache.org>
2025-05-18 08:07:46 -0400

    more stuff

--- cwazy.mjs
+++ cwazy.mjs
@@ -1,18 +1,19 @@
 const MORSE_CODE = {
-    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.',
-    'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
-    'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---',
-    'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
-    'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
-    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-',
-    '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
-    '/': '-..-.', 
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+    '/': '-..-.', '?': '..--..', '.': '.-.-.-', '!': '-.-.--',
+    'AR': '.-.-.', 'BK': '-...-.-',
 };
 
 export class Cwazy {
-    constructor(options = {}) {
+    constructor(options) {
+        options = options || {};
         this.wpm = options.wpm || 20;
         this.toneFreq = options.toneFreq || 600;
+        this.oscVolume = options.oscVolume || 0.9;
+        this.noiseVolume = options.noiseVolume || 0.9;
+        this.bandpassLowFreq = 300;
+        this.bandpassHighFreq = 900;
     }
 
     get wpm() {
@@ -28,14 +29,61 @@ export class Cwazy {
         this.betweenWords = this.dit * 7; 
     }
 
+    get oscVolume() {
+        return this._oscVolume;
+    }
+    set oscVolume(x) {
+        this._oscVolume = x;
+        if (this._audioContext && this._oscVolumeNode) {
+            this._oscVolumeNode.gain.setValueAtTime(x, this._audioContext.currentTime);
+        }
+    }
+
+    get noiseVolume() {
+        return this._noiseVolume;
+    }
+    set noiseVolume(x) {
+        this._noiseVolume = x;
+        if (this._audioContext && this._noiseGainNode) {
+            this._noiseGainNode.gain.setValueAtTime(x, this._audioContext.currentTime);
+        }
+    }
+
+    get toneFreq() {
+        return this._toneFreq;
+    }
+    set toneFreq(x) {
+        this._toneFreq = x;
+        if (this._audioContext && this._oscNode) {
+            this._oscNode.frequency.setValueAtTime(x, this._audioContext.currentTime);
+        }
+    }
+
+    get bandpassLowFreq() {
+        return this._bandpassLowFreq;
+    }
+    set bandpassLowFreq(x) {
+        this._bandpassLowFreq = x;
+        if (this._audioContext && this._highpassNode) {
+            this._highpassNode.frequency.setValueAtTime(x, this._audioContext.currentTime);
+        }
+    }
+    
+    get bandpassHighFreq() {
+        return this._bandpassHighFreq;
+    }
+    set bandpassHighFreq(x) {
+        this._bandpassHighFreq = x;
+        if (this._audioContext && this._lowpassNode) {
+            this._lowpassNode.frequency.setValueAtTime(x, this._audioContext.currentTime);
+        }
+    }
+
     async start() {
         if (this._audioContext) return;
 
         const audioContext = new AudioContext();
         this._audioContext = audioContext;
-
-        const FL = 300;
-        const FH = 800;
 
         await audioContext.audioWorklet.addModule("random-noise-processor.js");
         const randomNoiseNode = new AudioWorkletNode(
@@ -44,35 +92,40 @@ export class Cwazy {
         );
     
         const noiseGainNode = audioContext.createGain();
-        noiseGainNode.gain.setValueAtTime(0.9, audioContext.currentTime);
+        this._noiseGainNode = noiseGainNode;
+        noiseGainNode.gain.setValueAtTime(this._noiseVolume, audioContext.currentTime);
     
-        const oscillatorNode = audioContext.createOscillator();
-        oscillatorNode.frequency.setValueAtTime(600, audioContext.currentTime);
-        oscillatorNode.type = "sine";
-        oscillatorNode.start();
+        const oscNode = audioContext.createOscillator();
+        this._oscNode = oscNode;
+        oscNode.frequency.setValueAtTime(this.toneFreq, audioContext.currentTime);
+        oscNode.type = "sine";
+        oscNode.start();
+
+        const oscVolumeNode = audioContext.createGain();
+        this._oscVolumeNode = oscVolumeNode;
+        oscVolumeNode.gain.setValueAtTime(this.oscVolume, audioContext.currentTime);
     
         const oscGainNode = audioContext.createGain();
         this._oscGainNode = oscGainNode;
         oscGainNode.gain.setValueAtTime(0, audioContext.currentTime);
-        /*
-        for (let i=0; i<10; i++) {
-            oscGainNode.gain.setValueAtTime(1, audioContext.currentTime + 0.2 + 0.4*i);
-            oscGainNode.gain.setValueAtTime(0, audioContext.currentTime + 0.4 + 0.4*i);
-        }
-        */
-        
+
         const mergerNode = audioContext.createChannelMerger();
     
         const lowpassNode = audioContext.createBiquadFilter();
-        lowpassNode.frequency.setValueAtTime(FH, audioContext.currentTime);
+        this._lowpassNode = lowpassNode;
+        lowpassNode.type = "lowpass";
+        lowpassNode.frequency.setValueAtTime(this.bandpassHighFreq, audioContext.currentTime);
     
         const highpassNode = audioContext.createBiquadFilter();
-        highpassNode.frequency.setValueAtTime(FL, audioContext.currentTime);
+        this._highpassNode = highpassNode;
+        highpassNode.type = "highpass";
+        highpassNode.frequency.setValueAtTime(this.bandpassLowFreq, audioContext.currentTime);
     
         randomNoiseNode.connect(noiseGainNode);
         noiseGainNode.connect(mergerNode, 0, 0);
     
-        oscillatorNode.connect(oscGainNode);
+        oscNode.connect(oscVolumeNode);
+        oscVolumeNode.connect(oscGainNode);
         oscGainNode.connect(mergerNode, 0, 1);
     
         mergerNode.connect(lowpassNode);
@@ -84,6 +137,12 @@ export class Cwazy {
         if (this._audioContext === undefined) return;
         await this._audioContext.close();
         this._audioContext = undefined;
+        this._oscNode = undefined;
+        this._oscVolumeNode = undefined;
+        this._oscGainNode = undefined;
+        this._noiseGainNode = undefined;
+        this._lowpaasNode = undefined;
+        this._highpassNode = undefined;
     }
 
     sendText(text) {
--- main.js
+++ main.js
@@ -1,10 +1,51 @@
 import { Cwazy } from "./cwazy.mjs";
 
+function updateDisplays() {
+    const fields = ["wpm", "osc", "noise", "freq", "lowfreq", "highfreq"];
+    for (const field of fields) {
+        document.getElementById(`${field}-display`).innerHTML = document.getElementById(`${field}-input`).value;
+    }
+}
+updateDisplays();
+
 const cwazy = new Cwazy();
+
 document.getElementById("start-button").onclick = async () => await cwazy.start();
+
 document.getElementById("stop-button").onclick = async () => await cwazy.stop();
+
 document.getElementById("transmit-button").onclick = () => {
     const text = document.getElementById("transmit-text");
     cwazy.sendText(text.value);
+};
+
+document.getElementById("wpm-input").oninput = (e) => {
+    cwazy.wpm = parseInt(e.target.value, 10);
+    updateDisplays();
+};
+
+document.getElementById("osc-input").oninput = (e) => {
+    cwazy.oscVolume = parseFloat(e.target.value, 10);
+    updateDisplays();
+};
+
+document.getElementById("noise-input").oninput = (e) => {
+    cwazy.noiseVolume = parseFloat(e.target.value, 10);
+    updateDisplays();
+};
+
+document.getElementById("freq-input").oninput = (e) => {
+    cwazy.toneFreq = parseInt(e.target.value, 10);
+    updateDisplays();
+};
+
+document.getElementById("lowfreq-input").oninput = (e) => {
+    cwazy.bandpassLowFreq = parseInt(e.target.value, 10);
+    updateDisplays();
+};
+
+document.getElementById("highfreq-input").oninput = (e) => {
+    cwazy.bandpassHighFreq = parseInt(e.target.value, 10);
+    updateDisplays();
 };
 
